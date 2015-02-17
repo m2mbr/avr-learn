001bae8ab7b659ae9e551b1fa1f20c78ad7d236e
Sinval Júnior <nego@nego.local>
2015-02-16 23:29:24 -0200

    PWM

--- pro.cc
+++ pro.cc
@@ -4,16 +4,17 @@
 
 
 int main(void){
-    unsigned int i,j;
+    unsigned char i,j;
     unsigned int k;
+
     DDRB = 0xFF;
     PORTB |= 1 << PCINT5;
 
     for(;;){
 
-        for(j=0; j<150; j++){
+        for(j=0; j<50; j++){
             for(k=0; k<255; k++){
-                for(i=0; i<150; i++)
+                for(i=0; i<50; i++)
                 {
                     if(i < j){
                         PORTB |= 1 << PCINT5;
@@ -22,6 +23,19 @@ int main(void){
                     }
                 }
             }            
+        }
+
+        for(j=0;j<50;j++){
+            for(k=0;k<255;k++){
+                for(i=50;i>0;i--){
+                    if(i>j){
+                        PORTB |= 1 << PCINT5;
+                    }
+                    else{
+                        PORTB &= ~(1 << PCINT5);
+                    }
+                }
+            }
         }
 
     //tbi(PORTB, PCINT5);
