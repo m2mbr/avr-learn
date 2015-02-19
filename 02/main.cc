9b447d9d9250c0529f6a2e5634bdb44e98347058
Júnior <sinvalju@gmail.com>
2015-02-19 02:04:40 -0200

    while in loop

--- 02/main.cc
+++ 02/main.cc
@@ -12,7 +12,7 @@ int main(void){
     TCCR1B = _BV(CS10) | _BV(CS11)  | _BV(WGM12); // prescaler=64, clear timer/counter on compareA match                
     OCR1A = ((XTAL/64)/(2*TIMER_CLOCK)) - 1; // preset timer1 high/low byte
 
-    for(;;){
+    while(1){
 
     }
 
